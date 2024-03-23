1c3cc93dab270993528c4cfeb5c29b158ccad0b2
talatkuyuk <talatkuyuk@gmail.com>
2024-03-23 18:39:05 +0300

    added test coverage

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,14 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     include: ["tests/**/*.spec.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: [
+        ["lcov", { projectRoot: "./src" }],
+        ["json", { file: "coverage.json" }],
+        "text",
+      ],
+      exclude: ["archive", "tests", "**/*.d.ts"],
+    },
   },
 });
